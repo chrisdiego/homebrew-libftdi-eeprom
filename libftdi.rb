d16c7997791c43586139ffc9f81305b5043d2acb
Chris Diego <chrisdiego1@gmail.com>
2022-05-30 16:49:45 -0700

    Update libftdi.rb

--- libftdi.rb
+++ libftdi.rb
@@ -5,7 +5,6 @@ class Libftdi < Formula
   sha256 "a6ea795c829219015eb372b03008351cee3fb39f684bff3bf8a4620b558488d6"
 
   bottle do
-    cellar :any
     sha256 "81690eae8fa778df6a48557d03b154fbb3fe726d27edf8c8cccfb0f440810c46" => :el_capitan
     sha256 "fe679703a4c73cdae3d5c893c411ae4f080c113c9aef2e7c290c6c7386e2357d" => :yosemite
     sha256 "4049462256cac963b49c6cb430be7458cfad761af6db42cfef19ac33c8a8eca6" => :mavericks
